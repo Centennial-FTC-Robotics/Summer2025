c46f7769d18d0f555d287d485ad49b4cb0c3b3ab
JacobeZhang <jzhang5850@gmail.com>
2025-07-27 16:03:18 -0400

    Adding example code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/SampleTeleOp.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "SampleTeleOp")
+public class SampleTeleOp extends LinearOpMode {
+
+    private DcMotorEx driveBL;
+    private DcMotorEx driveBR;
+    private DcMotorEx driveFL;
+    private DcMotorEx driveFR;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        driveBL = hardwareMap.get(DcMotorEx.class, "backLeft");
+        driveBR = hardwareMap.get(DcMotorEx.class, "backRight");
+        driveFL = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        driveFR = hardwareMap.get(DcMotorEx.class, "frontRight");
+
+        driveBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        driveBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        driveBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // SWAP THESE IF NEEDED
+        driveBL.setDirection(DcMotorEx.Direction.REVERSE);
+        driveFL.setDirection(DcMotorEx.Direction.REVERSE);
+
+        while(opModeIsActive()) {
+            driveBL.setPower(1);
+            driveBR.setPower(1);
+            driveFL.setPower(1);
+            driveFR.setPower(1);
+            if (gamepad1.a) {
+                driveFL.setPower(0);
+                driveBL.setPower(0);
+                driveFL.setPower(0);
+
+            }
+        }
+    }
+}
